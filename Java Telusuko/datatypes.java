e3d98979410ba4b318c724e2e69b53ab4e739c44
NavaneethReddySeelam <seelamnbvnreddy@gmail.com>
2025-02-22 10:20:01 +0530

    java intial

---
+++ Java Telusuko/datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes{
+    public static void main(String[]args){
+        byte numm=9;// 1 byte
+        short twonum = 98;// 2 bytes
+        int num =1000;// 4 bytes
+        long num3 = 1000000000;// 8 bytes
+        double num1 = 10.99;// 8 bytes
+        float num2 = 10.3333f;//4 bytes
+        String name = "Java";// 2 bytes
+        char letter = 'J';// 2 bytes
+        char number = '4';// 2 bytes
+        boolean bool = true;// 1 byte
+
+    }
+}
---
+++ Java Telusuko/literals.java
@@ -0,0 +1,10 @@
+public class literals{
+    public static void main(String[]args){
+        int num = 101;
+        char c = 'j';
+        c++;
+        System.out.println("num: "+num);
+        System.out.println("c: "+c);
+
+    }
+}
